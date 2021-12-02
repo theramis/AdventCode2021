f9f871c22de2576724615017cfb898df29aee8e4
Simar Kalra <theramis@users.noreply.github.com>
2021-12-02 22:35:23 +1300

    Make day 1 a fancy 1 liner

--- src/AdventCode2021/Days/1/Day1Runner.cs
+++ src/AdventCode2021/Days/1/Day1Runner.cs
@@ -6,39 +6,13 @@ public class Day1Runner : IRunner
 
     public void RunPart1()
     {
-        var prev = _numbers[0];
-        var count = 0;
-        foreach (var number in _numbers[1..])
-        {
-            if (number > prev)
-                count++;
-
-            prev = number;
-        }
-
+        var count = _numbers[1..].Select((num, i) => num > _numbers[i]).Count(x => x);
         Console.WriteLine($"Part 1 Answer: {count}");
     }
 
     public void RunPart2()
     {
-        var prev1 = _numbers[0];
-        var prev2 = _numbers[1];
-        var prev3 = _numbers[2];
-
-        var count = 0;
-        foreach (var number in _numbers[3..])
-        {
-            var prevSum = prev1 + prev2 + prev3;
-            var currentSum = prev2 + prev3 + number;
-
-            if (currentSum > prevSum)
-                count++;
-
-            prev1 = prev2;
-            prev2 = prev3;
-            prev3 = number;
-        }
-
+        var count = _numbers[3..].Select((num, i) => num > _numbers[i]).Count(x => x);
         Console.WriteLine($"Part 2 Answer: {count}");
     }
 }
